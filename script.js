31a432ac863531cf75766a0adbce19a3587e3695
Mohammad Faseeh <145072594+MohammadFaseeh@users.noreply.github.com>
2025-07-28 23:14:23 +0500

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-const bar = document.getElementById('bar');
-const close = document.getElementById('close');
-const nav = document.getElementById('navbar');
+// Navbar bar icon close and open
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const nav = document.getElementById("navbar");
 
-if(bar){
-    bar.addEventListener('click', ()=> {
-        nav.classList.add('active');
-    })
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
 }
-if(close){
-    close.addEventListener('click', ()=> {
-        nav.classList.remove('active');
-    })
+if (close) {
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
+
+// Image Swap on Click
+var mainImg = document.getElementById("mainImg");
+
+var smallImgs = document.getElementsByClassName("small-img");
+
+for (let i = 0; i < smallImgs.length; i++) {
+  smallImgs[i].onclick = function () {
+    mainImg.src = smallImgs[i].src;
+  };
 }
